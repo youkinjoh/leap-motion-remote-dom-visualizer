79168c726bf888f717fba66b969c5b887f8f4d4a
you kinjoh <you.kinjoh@gmail.com>
2015-06-16 13:03:29 +0900

    add connection id function.

--- app.js
+++ app.js
@@ -10,14 +10,61 @@ app.use(express.static(__dirname + '/public'));
 var server = http.createServer(app);
 var wss = new WebSocketServer({server: server});
 
+var connectionIds = {};
+
 wss.on('connection', function connection(ws) {
+
+  var remoteIds = Object.keys(connectionIds);
+
+  var connectionId;
+  while(true) {
+    connectionId = Math.floor(Math.random()*0xffffffff).toString(16);
+    if (connectionId in connectionIds) {
+    } else {
+      connectionIds[connectionId] = true;
+      break;
+    }
+  }
+
+  ws.send(JSON.stringify({
+    messageType: 'connect',
+    connectionId: connectionId,
+    remoteIds: remoteIds
+  }));
+  wss.clients
+    .filter(function(client) {return client !== ws;})
+    .forEach(function(client) {
+      client.send(JSON.stringify({
+        messageType: 'connect_remote',
+        connectionId: connectionId
+      }));
+    });
+
   ws.on('message', function incoming(message) {
+    var data = JSON.stringify({
+      messageType: 'message',
+      connectionId: connectionId,
+      data: JSON.parse(message)
+    });
     wss.clients
       .filter(function(client) {return client !== ws;})
       .forEach(function(client) {
-        client.send(message);
+        client.send(data);
       });
   });
+
+  ws.on('close', function() {
+    delete connectionIds[connectionId];
+    wss.clients
+      .filter(function(client) {return client !== ws;})
+      .forEach(function(client) {
+        client.send(JSON.stringify({
+          messageType: 'disconnect_remote',
+          connectionId: connectionId
+        }));
+      });
+  });
+
 });
 
 server.listen(app.get('port'), function() {
